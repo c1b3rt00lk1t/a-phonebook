5b7075a26474d348af9902285a594eda0f4c9745
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-09-19 22:24:50 +0200

    Added a controlled value to the input and a onChange handler.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleInputNewName = (e) => {
+    e.preventDefault()
+    console.log(e.target.value)
+    setNewName(e.target.value)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input onChange={handleInputNewName} value={newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
