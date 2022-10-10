4bf5fd7110faa2d90745478bf5a88d0d51a1e0ea
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-10 20:14:36 +0200

    PersonForm logic extracted.

--- src/components/PersonForm.jsx
+++ src/components/PersonForm.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const PersonForm = () => {
+const PersonForm = ({handleInputNewName,newName,handleInputNewNumber,newNumber,handleClickAddName}) => {
   return (
-    <div>PersonForm</div>
+    <form>
+        <div>
+          name: <input onChange={handleInputNewName} value={newName} />
+        </div>
+        <div>
+          number: <input onChange={handleInputNewNumber} value={newNumber} />
+        </div>
+        <div>
+          <button onClick={handleClickAddName} type="submit">
+            add
+          </button>
+        </div>
+      </form>
   )
 }
 
