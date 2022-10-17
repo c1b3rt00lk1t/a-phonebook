bf5cf07ca5e28bb1d9376fdd6fb458e39f8b7493
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-18 00:00:26 +0200

    A check to prevent empty names has been put in place and the db.json cleaned up.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-import axios from 'axios'
+import axios from "axios";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -29,30 +29,31 @@ const App = () => {
 
   const handleClickAddName = (e) => {
     e.preventDefault();
-    if (!persons.filter((person) => person.name === newName)[0]) {
-      setPersons(persons.concat({ name: newName, number: newNumber }));
+    const newPerson = { name: newName, number: newNumber };
+    if (newPerson.name === ""){
+      alert('The name field is empty...');
+
+    } else if (!persons.filter((person) => person.name === newName)[0]) {
+      setPersons(persons.concat(newPerson));
       setNewName("");
       setNewNumber("");
+
+      axios
+        .post("http://localhost:3001/persons", newPerson)
+        .then((response) => console.log(response));
     } else {
       alert(`${newName} is already added to phonebook`);
     }
   };
 
-
   const getDataFromJSONServer = () => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
-      })
-  
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
   };
 
-  useEffect(getDataFromJSONServer,[])
-
-
-
+  useEffect(getDataFromJSONServer, []);
 
   return (
     <div>
