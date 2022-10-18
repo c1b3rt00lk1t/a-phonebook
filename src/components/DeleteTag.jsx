52a5d7f1d3faf65638d08d2db492fe0b403e18fc
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-18 14:22:03 +0200

    Basic styling of the deleteTag component.

--- src/components/DeleteTag.jsx
+++ src/components/DeleteTag.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DeleteTag = () => {
   return (
-    <div>deleteTag</div>
+    <span style={{border:"solid", borderRadius:"5px", borderWidth:"1px", padding:"0px 2px", fontSize:"10px", cursor:"pointer"}}> delete</span>
   )
 }
 
