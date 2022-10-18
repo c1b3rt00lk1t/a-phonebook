b2980f2e72f0477b94d9d71316b1f409ef326787
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-18 14:28:07 +0200

    Included expected inputs and the onClick handling functionality (for deletion).

--- src/components/DeleteTag.jsx
+++ src/components/DeleteTag.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const DeleteTag = () => {
+const DeleteTag = ({name, deletePerson}) => {
+
+  const handleClick = () => {
+    if (window.confirm(`Delete ${name}`)){
+      deletePerson(name);
+    }
+  }
+
   return (
-    <span style={{border:"solid", borderRadius:"5px", borderWidth:"1px", padding:"0px 2px", fontSize:"10px", cursor:"pointer"}}> delete</span>
+    <span onClick={handleClick} style={{border:"solid", borderRadius:"5px", borderWidth:"1px", padding:"0px 2px", fontSize:"10px", cursor:"pointer"}}> delete</span>
   )
 }
 
