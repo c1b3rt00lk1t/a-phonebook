6bf5e6a5ab90eadbaf94cb80e6d9d6b3da050e1f
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-18 14:18:38 +0200

    Included the row deleteTag in the persons list.

--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import DeleteTag from './DeleteTag'
 
 const Persons = ({filteredPersons}) => {
   return (
     <ul>
         {filteredPersons.map((person) => (
           <li key={person.name}>
-            {person.name} {person.number}
+            {person.name} {person.number} <DeleteTag />
           </li>
         ))}
       </ul>
