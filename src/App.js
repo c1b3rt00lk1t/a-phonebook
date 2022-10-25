28fb320ac7cf557b2d86078e9cf35a5e1d3eafdc
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-25 14:00:16 +0200

    Created the necessary states for determining the notification and displaying it. Added the funcionality when a new name is added.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-import { getAllAxios, createAxios,deleteAxios, updateAxios } from "./services/personsServer";
-import './index.css';
+import {
+  getAllAxios,
+  createAxios,
+  deleteAxios,
+  updateAxios,
+} from "./services/personsServer";
+import "./index.css";
 import Notification from "./components/Notification";
-
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -13,6 +17,10 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterName, setFilterName] = useState("");
+
+  const [notificationColor, setNotificationColor] = useState('');
+  const [notificationMessage, setNotificationMessage] = useState('');
+  const [showNotification, setShowNotification] = useState(false);
 
   const handleInputFilterName = (e) => {
     setFilterName(e.target.value);
@@ -33,40 +41,61 @@ const App = () => {
   const handleClickAddName = (e) => {
     e.preventDefault();
     const newPerson = { name: newName, number: newNumber };
-    const alreadyExistsPerson = persons.filter((person) => person.name === newName)[0];
+    const alreadyExistsPerson = persons.filter(
+      (person) => person.name === newName
+    )[0];
 
-    if (newPerson.name === ""){
-      alert('The name field is empty...');
-
+    if (newPerson.name === "") {
+      alert("The name field is empty...");
     } else if (!alreadyExistsPerson) {
       setNewName("");
       setNewNumber("");
 
-      createAxios(newPerson).then(response => setPersons(persons.concat(response)));
+      createAxios(newPerson)
+        .then((response) => {
+          setPersons(persons.concat(response));
+          setNotificationMessage(`Added ${response.name}`)
+          setNotificationColor('green');
+          setShowNotification(true);
+          setTimeout(() => setShowNotification(false), 4000)
+        })
     } else {
-      if(window.confirm(`Update ${newName}`)){
+      if (window.confirm(`Update ${newName}`)) {
         setNewName("");
         setNewNumber("");
-        updateAxios(alreadyExistsPerson.id, newPerson).then(response => setPersons(persons.map( person => person.id === response.id ? {...person, number: response.number} : person)));
+        updateAxios(alreadyExistsPerson.id, newPerson).then((response) =>{
+          setPersons(
+            persons.map((person) =>
+              person.id === response.id
+                ? { ...person, number: response.number }
+                : person
+            )
+          );
+        }
+        );
       }
     }
   };
 
   const deletePerson = (id) => {
     deleteAxios(id);
-    setPersons(persons.filter( person => person.id !== id));
-  }
+    setPersons(persons.filter((person) => person.id !== id));
+  };
 
   const getPersonsFromServer = () => {
-    getAllAxios().then(response => setPersons(response));
-   };
+    getAllAxios().then((response) => setPersons(response));
+  };
 
   useEffect(getPersonsFromServer, []);
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Notification message={"This will be a notification"} color={"green"} display={true}/>
+      <Notification
+        message={notificationMessage}
+        color={notificationColor}
+        display={showNotification}
+      />
       <Filter
         handleInputFilterName={handleInputFilterName}
         filterName={filterName}
@@ -82,7 +111,7 @@ const App = () => {
       />
 
       <h2>Numbers</h2>
-      <Persons filteredPersons={filteredPersons} deletePerson={deletePerson}/>
+      <Persons filteredPersons={filteredPersons} deletePerson={deletePerson} />
     </div>
   );
 };
