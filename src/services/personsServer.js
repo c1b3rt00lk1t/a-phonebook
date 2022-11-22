211f9dceb55ac9f27293114a4c0b06452014303d
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-11-22 08:09:15 +0100

    Adjusted the base url so that it is relative.

--- src/services/personsServer.js
+++ src/services/personsServer.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:8080/api/persons/';
+const baseUrl = '/api/persons/';
 
 export const getAllAxios = async () => {
     const request = axios.get(baseUrl); 
